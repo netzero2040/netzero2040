db35970c6454a3931ab84f9b5a3119c6617cc23d
Daniel Huppmann <dh@dergelbesalon.at>
2022-07-22 21:40:42 +0200

    Add validation to workflow

--- workflow.py
+++ workflow.py
@@ -1,5 +1,22 @@
 import pyam
+from nomenclature import DataStructureDefinition, process
+from pathlib import Path
 
-def main(df: pyam.IamDataFrame) -> pyam.IamDataFrame:   
+here = Path(__file__).absolute().parent
+
+
+def main(df: pyam.IamDataFrame) -> pyam.IamDataFrame:
+    """Project/instance-specific workflow for scenario processing"""
+
+    # Cast to an IamDataFrame if necessary
+    if not isinstance(df, pyam.IamDataFrame):
+        df = pyam.IamDataFrame(df)
+
+    # Import the codelists for the project
+    definition = DataStructureDefinition(here / "definitions")
+
+    # Validate `df` against the codelists
+    # This method raises an error if any items are not defined in the codelists.
+    definition.validate(df)
+
     return df
-  
